306cab4ae31eff2f6445c3dc6cd4931390f1b158
jerome-qn <106096008+jerome-qn@users.noreply.github.com>
2022-06-17 10:33:22 -0700

    remove unused imports; remove poetry.lock from gitignore

--- src/openrarity/scoring/distance_metric.py
+++ src/openrarity/scoring/distance_metric.py
@@ -1,7 +1,5 @@
-from dataclasses import dataclass
 from models.token import Token
 from base import BaseRarityFormula
-import numpy as np
 
 class DistanceMetricRarity(BaseRarityFormula):
     """the mean number of attribute swaps required to reach another token"""
