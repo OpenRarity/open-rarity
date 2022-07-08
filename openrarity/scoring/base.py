84d337884283e7fe594420d51a11920728aa1dbe
jerome-qn <106096008+jerome-qn@users.noreply.github.com>
2022-07-08 10:40:05 -0700

    update arguments in base.py

--- openrarity/scoring/base.py
+++ openrarity/scoring/base.py
@@ -5,19 +5,23 @@ from openrarity.models.token import Token
 class BaseRarityFormula:
     """base rarity class"""
 
-    def score_token(self, token: Token) -> float:
+    def score_token(self, token: Token, normalized: bool) -> float:
         raise NotImplementedError
 
     # base aggregate scorers: can override w/ more efficient methods
 
-    def score_tokens(self, tokens: list[Token]) -> list[float]:
+    def score_tokens(
+        self, tokens: list[Token], normalized: bool
+    ) -> list[float]:
         return [self.score_token(t) for t in tokens]
 
-    def score_collection(self, collection: Collection) -> list[float]:
+    def score_collection(
+        self, collection: Collection, normalized: bool
+    ) -> list[float]:
         tokens = collection.tokens
         return [self.score_token(t) for t in tokens]
 
     def score_collections(
-        self, collections: list[Collection]
+        self, collections: list[Collection], normalized: bool
     ) -> list[list[float]]:
         return [self.score_collection(c) for c in collections]
