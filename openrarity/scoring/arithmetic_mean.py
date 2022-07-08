a427e145bc5accced928dca2ea1629644daaf6e5
jerome-qn <106096008+jerome-qn@users.noreply.github.com>
2022-07-08 10:23:00 -0700

    add normalized rarity formulae

--- openrarity/scoring/arithmetic_mean.py
+++ openrarity/scoring/arithmetic_mean.py
@@ -1,6 +1,6 @@
 import numpy as np
-from openrarity.scoring.base import BaseRarityFormula
-from openrarity.scoring.utils import get_attr_probs
+from base import BaseRarityFormula
+from utils import get_attr_probs_weights
 
 from openrarity.models.token import Token
 
@@ -8,11 +8,9 @@ from openrarity.models.token import Token
 class ArithmeticMeanRarity(BaseRarityFormula):
     """arithmetic mean of a token's n trait probabilities"""
 
-    def score_token(self, token: Token) -> float:
+    def score_token(self, token: Token, normalized: bool = False) -> float:
         """calculate the score for a single token"""
 
-        string_attr_list = list(token.metadata.string_attributes.values())
+        attr_probs, attr_weights = get_attr_probs_weights(token, normalized)
 
-        attr_probs = get_attr_probs(string_attr_list, token)
-
-        return np.mean(attr_probs)
+        return np.average(attr_probs, weights=attr_weights)
--- openrarity/scoring/utils.py
+++ openrarity/scoring/utils.py
@@ -1,11 +1,22 @@
 from openrarity.models.token import Token
-from openrarity.models.token_metadata import StringAttributeValue
 
 
-def get_attr_probs(
-    string_attr_list: list[StringAttributeValue], token: Token
-) -> list[float]:
-    """get list of attribute probabilities"""
+def get_attr_probs_weights(
+    token: Token, normalized: bool
+) -> list[list[float], list[float]]:
+    """get attribute probabilities & weights"""
 
+    string_attr_keys = sorted(list(token.metadata.string_attributes.keys()))
+    string_attr_list = [
+        token.metadata.string_attributes[k] for k in string_attr_keys
+    ]
     supply = token.collection.token_total_supply
-    return [attr.count / supply for attr in string_attr_list]
+
+    attr_weights = [1] * len(string_attr_keys)
+    if normalized:
+        attr_weights = [
+            1 / len(token.collection.attributes_count[k])
+            for k in string_attr_keys
+        ]
+
+    return [[attr.count / supply for attr in string_attr_list], attr_weights]
