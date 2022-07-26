Fixes for the information content rarity computation

openrarity/scoring/information_content.py
@@ -5,7 +5,6 @@ from openrarity.models.token import Token
 from openrarity.models.token_metadata import StringAttributeValue
 from openrarity.scoring.base import BaseRarityFormula
 from openrarity.scoring.utils import get_attr_probs_weights
-import scipy
 
 logger = logging.getLogger("open_rarity_logger")
 
@@ -17,6 +16,11 @@ class InformationContentRarity(BaseRarityFormula):
     def score_token(self, token: Token, normalized: bool = True) -> float:
         """calculate the score for a single token"""
 
+        logger.debug(
+            "Computing InformationContent for token {id}".format(
+                id=token.token_id
+            )
+        )
         # Scores are already inverted probabilities ,
         # We need to take sum of logarithms to estimate
         # information content.
@@ -27,15 +31,19 @@ class InformationContentRarity(BaseRarityFormula):
         )
         logger.debug(
             "Collection_probabilities {probs}".format(
-                probs=collection_probabilities
+                probs=collection_probabilities,
             )
         )
 
         # Scores are already inverted probabilities ,
         # We need to take sum of logarithms to estimate
         # information content.
-        information_content = sum(np.log2(scores))
-        collection_entropy = scipy.stats.entropy(collection_probabilities)
+        information_content = -sum(np.log2(np.reciprocal(scores)))
+
+        # compute entropy for the whole collection
+        collection_entropy = -np.dot(
+            collection_probabilities, np.log2(collection_probabilities)
+        )
 
         logger.debug(
             "Information content {probs}".format(probs=information_content)
@@ -43,7 +51,7 @@ class InformationContentRarity(BaseRarityFormula):
 
         logger.debug(
             "Collection {collection} entropy {probs}".format(
-                collection=token.collection.name, probs=information_content
+                collection=token.collection.name, probs=collection_entropy
             )
         )
 
@@ -52,7 +60,7 @@ class InformationContentRarity(BaseRarityFormula):
     def get_collection_probabilities(self, collection: Collection):
         collection_attributes: dict[
             str, list[StringAttributeValue]
-        ] = collection.extract_collection_attributes
+        ] = collection.extract_collection_attributes()
         collection_null_attributes: dict[
             str, StringAttributeValue
         ] = collection.extract_null_attributes
@@ -60,9 +68,14 @@ class InformationContentRarity(BaseRarityFormula):
         # collect all probabilities into array
         collection_probabilities = []
         for value, _ in collection_attributes.items():
-            collection_attributes[value].append(
+            null_attr = (
                 collection_null_attributes[value]
+                if value in collection_null_attributes
+                else None
             )
+
+            if null_attr:
+                collection_attributes[value].append(null_attr)
 
             collection_probabilities.extend(
                 [
@@ -70,4 +83,5 @@ class InformationContentRarity(BaseRarityFormula):
                     for value in collection_attributes[value]
                 ]
             )
+
         return collection_probabilities

openrarity/tests/test_scoring.py
@@ -102,14 +102,14 @@ class TestScorring:
         information_content_rarity = InformationContentRarity()
 
         uniform_token_to_test = self.uniform_collection.tokens[0]
-        uniform_ic_rarity = 4.24579315
+        uniform_ic_rarity = 1.0
         assert np.round(
             information_content_rarity.score_token(uniform_token_to_test),
             8,
         ) == np.round(uniform_ic_rarity, 8)
 
         onerare_token_to_test = self.onerare_collection.tokens[0]
-        onerare_ic_mean = 4.20697466
+        onerare_ic_mean = 0.99085719
         assert np.round(
             information_content_rarity.score_token(onerare_token_to_test),
             8,