0f3f3ccf071c761c56eda3013324008f0250c49d
vickygos <93538907+vickygos@users.noreply.github.com>
2022-09-14 13:46:41 -0700

    added token erc721 factory method

--- open_rarity/models/token.py
+++ open_rarity/models/token.py
@@ -1,7 +1,11 @@
 from dataclasses import dataclass
+from typing import Any
 
-from open_rarity.models.token_identifier import TokenIdentifier
-from open_rarity.models.token_metadata import TokenMetadata
+from open_rarity.models.token_identifier import (
+    EVMContractTokenIdentifier,
+    TokenIdentifier,
+)
+from open_rarity.models.token_metadata import AttributeName, TokenMetadata
 from open_rarity.models.token_standard import TokenStandard
 
 
@@ -24,6 +28,46 @@ class Token:
     token_identifier: TokenIdentifier
     token_standard: TokenStandard
     metadata: TokenMetadata
+
+    @classmethod
+    def from_erc721(
+        cls,
+        contract_address: str,
+        token_id: int,
+        metadata_dict: dict[AttributeName, Any],
+    ):
+        """Creates a Token class representing an ERC721 evm token given the following
+        parameters.
+
+        Parameters
+        ----------
+        contract_address : str
+            Contract address of the token
+        token_id : int
+            Token ID number of the token
+        metadata_dict : dict
+            Dictionary of attribute name to attribute value for the given token.
+            The type of the value determines whether the attribute is a string,
+            numeric or date attribute.
+
+            class           attribute type
+            ------------    -------------
+            string          string attribute
+            int | float     numeric_attribute
+            datetime        date_attribute (stored as timestamp, seconds from epoch)
+
+        Returns
+        -------
+        Token
+            A Token instance with EVMContractTokenIdentifier and ERC721 standard set.
+        """
+        return cls(
+            token_identifier=EVMContractTokenIdentifier(
+                contract_address=contract_address, token_id=token_id
+            ),
+            token_standard=TokenStandard.ERC721,
+            metadata=TokenMetadata.from_attributes(metadata_dict),
+        )
 
     def __str__(self):
         return f"Token[{self.token_identifier}]"
--- scripts/score_generated_collection.py
+++ scripts/score_generated_collection.py
@@ -1,58 +1,29 @@
 from open_rarity import (
     Collection,
-    EVMContractTokenIdentifier,
     OpenRarityScorer,
-    StringAttribute,
     Token,
-    TokenMetadata,
-    TokenStandard,
 )
 
 if __name__ == "__main__":
     scorer = OpenRarityScorer()
-    # A collection of 2 tokens
+
     collection = Collection(
         name="My Collection Name",
-        attributes_frequency_counts={
-            "hat": {"cap": 1, "visor": 2},
-            "shirt": {"blue": 2, "green": 1},
-        },
         tokens=[
-            Token(
-                token_identifier=EVMContractTokenIdentifier(
-                    contract_address="0xa3049...", token_id=1
-                ),
-                token_standard=TokenStandard.ERC721,
-                metadata=TokenMetadata(
-                    string_attributes={
-                        "hat": StringAttribute(name="hat", value="cap"),
-                        "shirt": StringAttribute(name="shirt", value="blue"),
-                    }
-                ),
+            Token.from_erc721(
+                contract_address="0xa3049...",
+                token_id=1,
+                metadata_dict={"hat": "cap", "shirt": "blue"},
             ),
-            Token(
-                token_identifier=EVMContractTokenIdentifier(
-                    contract_address="0xa3049...", token_id=2
-                ),
-                token_standard=TokenStandard.ERC721,
-                metadata=TokenMetadata(
-                    string_attributes={
-                        "hat": StringAttribute(name="hat", value="visor"),
-                        "shirt": StringAttribute(name="shirt", value="green"),
-                    }
-                ),
+            Token.from_erc721(
+                contract_address="0xa3049...",
+                token_id=2,
+                metadata_dict={"hat": "visor", "shirt": "green"},
             ),
-            Token(
-                token_identifier=EVMContractTokenIdentifier(
-                    contract_address="0xa3049...", token_id=3
-                ),
-                token_standard=TokenStandard.ERC721,
-                metadata=TokenMetadata(
-                    string_attributes={
-                        "hat": StringAttribute(name="hat", value="visor"),
-                        "shirt": StringAttribute(name="shirt", value="blue"),
-                    }
-                ),
+            Token.from_erc721(
+                contract_address="0xa3049...",
+                token_id=3,
+                metadata_dict={"hat": "visor", "shirt": "blue"},
             ),
         ],
     )  # Replace inputs with your collection-specific details here
