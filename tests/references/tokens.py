9977b026857fe9f0592331cb7c7a504b4590c278
Grant Li <grant.li@joinozone.com>
2022-11-10 10:57:25 -0500

    unskip token tests

--- tests/references/tokens.py
+++ tests/references/tokens.py
@@ -1,143 +1,275 @@
-# from inspect import cleandoc
+from inspect import cleandoc
 
-# from openrarity.token.token import (
-#     MetadataAttributeModel,
-#     NonFungibleTokenModel,
-#     SemiFungibleTokenModel,
-# )
 
-# # type: ignore
-# SUCCEEDS = [
-#     {
-#         "desc": cleandoc(
-#             """
-#             """
-#         ),
-#         "model": NonFungibleTokenModel,
-#         "input": {
-#             "attributes": [
-#                 {"trait_type": "hat", "value": "baseball", "display_type": "string"},
-#                 {"name": "shirt", "value": "blue", "display_type": "string"},
-#                 {"name": "shirt", "value": "jacket", "display_type": "string"},
-#                 {"name": "pants", "value": "cargo shorts"},
-#             ]
-#         },
-#         "_expected": {
-#             "attributes": sorted(
-#                 [
-#                     {"name": "shirt", "value": "jacket", "display_type": "string"},
-#                     {
-#                         "name": "pants",
-#                         "value": "cargo shorts",
-#                         "display_type": "string",
-#                     },
-#                     {"name": "shirt", "value": "blue", "display_type": "string"},
-#                     {"name": "hat", "value": "baseball", "display_type": "string"},
-#                     {
-#                         "name": "openrarity.trait_count",
-#                         "value": 4,
-#                         "display_type": "string",
-#                     },
-#                 ],
-#                 key=lambda attr: (attr["name"], attr["value"]),
-#             )
-#         },
-#     },
-#     {
-#         "desc": cleandoc(
-#             """
-#             """
-#         ),
-#         "model": SemiFungibleTokenModel,
-#         "input": {
-#             "token_supply": 10000,
-#             "attributes": [
-#                 {"trait_type": "hat", "value": "baseball", "display_type": "string"},
-#                 {"name": "shirt", "value": "blue", "display_type": "string"},
-#                 {"name": "shirt", "value": "jacket", "display_type": "string"},
-#                 {"name": "pants", "value": "cargo shorts"},
-#             ],
-#         },
-#         "_expected": {
-#             "token_supply": 10000,
-#             "attributes": sorted(
-#                 [
-#                     {"name": "shirt", "value": "jacket", "display_type": "string"},
-#                     {
-#                         "name": "pants",
-#                         "value": "cargo shorts",
-#                         "display_type": "string",
-#                     },
-#                     {"name": "shirt", "value": "blue", "display_type": "string"},
-#                     {"name": "hat", "value": "baseball", "display_type": "string"},
-#                     {
-#                         "name": "openrarity.trait_count",
-#                         "value": 4,
-#                         "display_type": "string",
-#                     },
-#                 ],
-#                 key=lambda attr: (attr["name"], attr["value"]),
-#             ),
-#         },
-#     },
-#     {
-#         "desc": cleandoc(
-#             """
-#             """
-#         ),
-#         "model": MetadataAttributeModel,
-#         "input": {"trait_type": "hat", "value": "baseball", "display_type": "string"},
-#         "_expected": {
-#             "name": "hat",
-#             "value": "baseball",
-#             "display_type": "string",
-#         },
-#     },
-#     {
-#         "desc": cleandoc(
-#             """
-#             """
-#         ),
-#         "model": MetadataAttributeModel,
-#         "input": {"trait_type": "animal", "value": "cat", "display_type": "Meow"},
-#         "_expected": {
-#             "name": "animal",
-#             "value": "cat",
-#             "display_type": "string",
-#         },
-#     },
-# ]
-# # type: ignore
-# FAILS = [  # type: ignore
-#     {
-#         "desc": cleandoc(
-#             """
-#             """
-#         ),
-#         "model": NonFungibleTokenModel,
-#         "input": {},
-#         "_expected": KeyError,
-#     },
-#     {
-#         "desc": cleandoc(
-#             """
-#             """
-#         ),
-#         "model": SemiFungibleTokenModel,
-#         "input": {
-#             "attributes": [
-#                 {"trait_type": "hat", "value": "baseball", "display_type": "string"},
-#             ],
-#         },
-#         "_expected": ValidationError,
-#     },
-#     {
-#         "desc": cleandoc(
-#             """
-#             """
-#         ),
-#         "model": MetadataAttributeModel,
-#         "input": {"trait_name": "hat", "value": "baseball", "display_type": "string"},
-#         "_expected": ValidationError,
-#     },
-# ]
+SUCCEEDS = [
+    {
+        "desc": cleandoc(
+            """Non-fungible tokens with string traits."""
+        ),
+        "input": {
+            "token_type": "non-fungible",
+            "tokens": {
+                1: {
+                    "attributes": [
+                        {"name": "weapon", "value": "axe"},
+                        {"name": "Animated ", "value": "No "},
+                    ]
+                },
+                2: {
+                    "attributes": [
+                        {"name": "weapon", "value": "kitana"},
+                        {"name": "Animated ", "value": "No "},
+                    ]
+                }
+            }
+        },
+        "_expected": {
+            "token_supply": 2,
+            "tokens": {
+                1: {
+                    'attributes': [
+                        {
+                            'name': 'animated',
+                            'value': 'no',
+                            'display_type': 'string',
+                        },
+                        {
+                            'name': 'weapon',
+                            'value': 'axe',
+                            'display_type': 'string',
+                        },
+                        {
+                            'name': 'openrarity.trait_count',
+                            'value': 2,
+                            'display_type': 'string',
+                        }
+                    ]
+                },
+                2: {
+                    'attributes': [
+                        {
+                            'name': 'animated',
+                            'value': 'no',
+                            'display_type': 'string',
+                        },
+                        {
+                            'name': 'weapon',
+                            'value': 'kitana',
+                            'display_type': 'string',
+                        },
+                        {
+                            'name': 'openrarity.trait_count',
+                            'value': 2,
+                            'display_type': 'string',
+                        }
+                    ]
+                }
+            }
+        }
+    },
+    {
+        "desc": cleandoc(
+            """Multiple values under same trait type."""
+        ),
+        "input": {
+            "token_type": "non-fungible",
+            "tokens": {
+                1: {
+                    "attributes": [
+                        {"name": "weapon", "value": "axe"},
+                        {"name": "weapon", "value": "kitana"},
+                    ]
+                }
+            },
+        },
+        "_expected": {
+            "token_supply": 1,
+            "tokens": {
+                1: {
+                    "attributes": [
+                        {
+                            "name": "weapon",
+                            "value": "axe",
+                            "display_type": "string",
+                        },
+                        {
+                            "name": "weapon",
+                            "value": "kitana",
+                            "display_type": "string",
+                        },
+                        {
+                            "name": "openrarity.trait_count",
+                            "value": 2,
+                            "display_type": "string",
+                        },
+                    ]
+                }
+            }
+        }
+    },
+    {
+        "desc": cleandoc(
+            """Duplicate traits."""
+        ),
+        "input": {
+            "token_type": "non-fungible",
+            "tokens": {
+                1: {
+                    "attributes": [
+                        {"name": "weapon", "value": "axe"},
+                        {"name": "weapon", "value": "axe"},
+                    ]
+                }
+            }
+        },
+        "_expected": {
+            "token_supply": 1,
+            "tokens": {
+                1: {
+                    "attributes": [
+                        {
+                            "name": "weapon",
+                            "value": "axe",
+                            "display_type": "string",
+                        },
+                        {
+                            "name": "openrarity.trait_count",
+                            "value": 1,
+                            "display_type": "string",
+                        },
+                    ]
+                }
+            }
+        }
+    },
+    {
+        "desc": cleandoc(
+            """Semi-fungible tokens."""
+        ),
+        "input": {
+            "token_type": "semi-fungible",
+            "tokens": {
+                1: {
+                    "token_supply": 5,
+                    "attributes": [
+                        {"name": "medal", "value": "gold"}
+                    ]
+                },
+                2: {
+                    "token_supply": 10,
+                    "attributes": [
+                        {"name": "medal", "value": "silver"}
+                    ]
+                }
+            }
+        },
+        "_expected": {
+            "token_supply": {
+                1: 5,
+                2: 10,
+            },
+            "tokens": {
+                1: {
+                    'token_supply': 5,
+                    'attributes': [
+                        {
+                            'name': 'medal',
+                            'value': 'gold',
+                            'display_type': 'string',
+                        },
+                        {
+                            'name': 'openrarity.trait_count',
+                            'value': 1,
+                            'display_type': 'string',
+                        }
+                    ]
+                },
+                2: {
+                    'token_supply': 10,
+                    'attributes': [
+                        {
+                            'name': 'medal',
+                            'value': 'silver',
+                            'display_type': 'string',
+                        },
+                        {
+                            'name': 'openrarity.trait_count',
+                            'value': 1,
+                            'display_type': 'string',
+                        }
+                    ]
+                }
+            }
+        },
+    },
+    {
+        "desc": cleandoc(
+            """Numeric and date traits"""
+        ),
+        "input": {
+            "token_type": "non-fungible",
+            "tokens": {
+                1: {
+                    "attributes": [
+                        {
+                            "name": "level",
+                            "value": 10,
+                            "display_type": "number",
+                        },
+                        {
+                            "name": "birthday",
+                            "value": 1647519737,
+                            "display_type": "date",
+                        }
+                    ]
+                },
+            }
+        },
+        "_expected": {
+            "token_supply": 1,
+            "tokens": {
+                1: {
+                    'attributes': [
+                        {
+                            'name': 'level',
+                            'value': 10.0,
+                            'display_type': 'number',
+                        },
+                        {
+                            'name': 'birthday',
+                            'value': 1647519737,
+                            'display_type': 'date',
+                        },
+                        {
+                            'name': 'openrarity.trait_count',
+                            'value': 2,
+                            'display_type': 'string',
+                        }
+                    ]
+                }
+            }
+        }
+    }
+]
+
+
+FAILS = [
+    {
+        "desc": cleandoc(
+            """Semi-fungible missing token supply"""
+        ),
+        "input": {
+            "token_type": "semi-fungible",
+            "tokens": {
+                1: {
+                    "attributes": [
+                        {"name": "medal", "value": "gold"}
+                    ]
+                }
+            }
+        },
+        "_expected": {
+            "expected_exception": ValueError, "match": "token_supply"
+        },
+    },
+]
--- tests/test_token.py
+++ tests/test_token.py
@@ -1,28 +1,32 @@
 # type: ignore
-# import pytest
-#
-# from openrarity.token.token import NonFungibleTokenModel, SemiFungibleTokenModel
-#
-# from .references.tokens import FAILS, SUCCEEDS
-#
-# _params = SUCCEEDS
-#
-#
-# @pytest.mark.parametrize("params", _params)
-# def test_init_model_succeeds(params):
-#
-#     validated = params["model"](**params["input"]).dict()
-#     if params["model"] in [NonFungibleTokenModel, SemiFungibleTokenModel]:
-#         validated["attributes"] = sorted(
-#             validated["attributes"], key=lambda attr: (attr["name"], attr["value"])
-#         )
-#     assert validated == params["_expected"]
-#
-#
-# _params = FAILS
-#
-#
-# @pytest.mark.parametrize("params", _params)
-# def test_init_model_fails(params):
-#     with pytest.raises(params["_expected"]):
-#         params["model"](**params["input"])
+import pytest
+
+from openrarity.token.token import validate_tokens
+
+from .references.tokens import SUCCEEDS, FAILS
+
+
+@pytest.mark.parametrize("params", SUCCEEDS)
+def test_validate_tokens_succeed(params):
+    token_supply, tokens = validate_tokens(**params["input"])
+    assert token_supply == params["_expected"]["token_supply"]
+
+    expected_tokens = params["_expected"]["tokens"]
+    assert tokens.keys() == expected_tokens.keys()
+    for token_id, token in tokens.items():
+        expected_token = expected_tokens[token_id]
+        if params["input"]["token_type"] == "semi-fungible":
+            assert token["token_supply"] == expected_token["token_supply"]
+        sorted_attrs = sorted(
+            token["attributes"], key=lambda a: sorted(a.items())
+        )
+        expected_sorted_attrs = sorted(
+            expected_token["attributes"], key=lambda a: sorted(a.items())
+        )
+        assert sorted_attrs == expected_sorted_attrs
+
+
+@pytest.mark.parametrize("params", FAILS)
+def test_validate_tokens_fail(params):
+    with pytest.raises(**params["_expected"]):
+        validate_tokens(**params["input"])
