c6f89314b024aa6d37edd3a1aaebcd65cf4be6f2
Damjan Kuznar <damjan.kuznar@gmail.com>
2023-07-25 07:41:51 +0200

    fix nox call in GH workflow

--- noxfile.py
+++ noxfile.py
@@ -6,4 +6,4 @@ from nox import Session
 @nox.parametrize("pydantic", ["1.9.1", "2.0.3"])
 def tests(session: Session, pydantic):
     session.install(f"pydantic=={pydantic}")
-    session.run("poetry", "run", "pytest")
+    session.run("pytest")
