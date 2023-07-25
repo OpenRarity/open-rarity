8af5ef6a7172d770a71afdc0929f81f3882bc54e
Damjan Kuznar <damjan.kuznar@gmail.com>
2023-07-25 07:29:47 +0200

    use nox

---
+++ noxfile.py
@@ -0,0 +1,9 @@
+import nox
+from nox import Session
+
+
+@nox.session(python=["3.10", "3.11"])
+@nox.parametrize("pydantic", ["1.9.1", "2.0.3"])
+def tests(session: Session, pydantic):
+    session.install(f'pydantic=={pydantic}')
+    session.run("poetry", "run", "pytest")
