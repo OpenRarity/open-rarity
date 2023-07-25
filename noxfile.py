4c8ba57cda9d9a3cc5d8511581335a156658554e
Damjan Kuznar <damjan.kuznar@gmail.com>
2023-07-25 08:32:28 +0200

    reuse venv

--- noxfile.py
+++ noxfile.py
@@ -2,7 +2,7 @@ import nox
 from nox import Session
 
 
-@nox.session(python=["3.10", "3.11"])
+@nox.session(python=["3.10", "3.11"], reuse_venv=True)
 @nox.parametrize("pydantic", ["1.9.1", "2.0.3"])
 def tests(session: Session, pydantic):
     session.install("poetry")
